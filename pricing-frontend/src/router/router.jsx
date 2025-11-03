2142e913772eea02f580e2c46ec5aa8d138a9f9e
Vivek Parihar <vivekparihar400@gmail.com>
2025-11-03 23:53:44 +0530

    backend code

--- pricing-frontend/src/router/router.jsx
+++
@@ -1,9 +0,0 @@
-import routes from "./routes";
-
-import { useRoutes } from "react-router-dom";
-
-function AppRouter() {
-  return useRoutes(routes);
-}
-
-export default AppRouter;
--- pricing-frontend/src/router/routes.js
+++
@@ -1,7 +0,0 @@
-import dashboardRoutes from "./dashboardRoutes";
-import loginRoute from "./loginRoute";
-import signupRoute from "./signupRoute";
-
-const routes = [...loginRoute, ...signupRoute, ...dashboardRoutes];
-
-export default routes;
